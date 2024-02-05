1c71d2977508f0f5422873f5c33ef71f91a10d37
Marco Wittig <marco-wittig@gmx.de>
2024-02-05 21:03:40 +0100

    Add example for sound

---
+++ src/main/java/io/github/mwttg/pixelatillery/example2/Application.java
@@ -0,0 +1,24 @@
+package io.github.mwttg.pixelatillery.example2;
+
+import io.github.mwttg.pixelartillery2d.sound.Sound;
+import io.github.mwttg.pixelartillery2d.sound.SoundDevice;
+import org.joml.Vector3f;
+
+public class Application {
+
+    public static void main(String[] args) throws InterruptedException {
+        SoundDevice.initialize();
+        // SoundListener exits by default, to manipulate it use the class SoundListener
+
+        final var sound = Sound.create("./data/sound/jump.wav", true);
+        var position = new Vector3f(-10.0f, 0.0f, -5.0f);
+
+        sound.play();
+        while (position.x < 10.0f) {
+            position.add(0.2f, 0.0f, 0.0f);
+            sound.updatePosition(position);
+            Thread.sleep(50);
+        }
+    }
+}
+
